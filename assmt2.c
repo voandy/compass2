e2392ade6c6043f976a28be4253f4ee24331a1e1
Andy <andy@voandy.com>
2017-05-06 14:36:34 +1000

    initialise project

---
+++ assmt2.c
@@ -0,0 +1,22 @@
+/* Programmer: Andy Vo, voa1@student.unimelb.edu.au
+
+   Algorithms are fun!
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct skyline_point_t{
+	int ref_no;
+	double coords[MAX_NUM_DIMENSIONS];
+	int points_dominated;
+} skyline_point_t;
+
+/* function prototypes */
+
+
+/* main program binds it all together */
+int
+main(int argc, char *argv[]) {
+	return 0;
+}
